53395907bfb9939e44413ed6886ac81e85ee8ab7
juanbono <juanbono94@gmail.com>
2023-10-03 17:08:09 -0300

    add blake2 program

--- risc0/fib/methods/guest/src/main.rs
+++ risc0/fib/methods/guest/src/main.rs
@@ -2,27 +2,15 @@
 // If you want to try std support, also update the guest Cargo.toml file
 #![no_std]  // std support is experimental
 
+use blake2::{Blake2s256, Digest};
 
 use risc0_zkvm::guest::env;
 
 risc0_zkvm::guest::entry!(main);
 pub fn main() {
-    // Load the first number from the host
-    let mut x0: u64 = 0;
-    // Load the second number from the host
-    let mut x1: u64 = 1;
-    let mut fib_acc: u64 = x0 + x1;
-    let mut n = 1000;
+    let mut hasher = Blake2s256::new();
+    hasher.update(b"hello world");
+    let res = hasher.finalize();
 
-    loop {
-        x0 = x1;
-        x1 = fib_acc;
-        fib_acc = x0 + x1;
-        n = n - 1;
-        if n == 0 {
-            break
-        }
-    }
-
-    env::commit(&fib_acc);
+    env::commit(&res.as_slice());
 }
