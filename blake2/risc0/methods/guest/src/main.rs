42513500ef857da8140def029752f0cd05cf32d1
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2023-10-06 16:49:55 -0300

    Add more hashes to programs

--- blake2/risc0/methods/guest/src/main.rs
+++ blake2/risc0/methods/guest/src/main.rs
@@ -11,18 +11,18 @@ risc0_zkvm::guest::entry!(main);
 pub fn main() { // program without loops
     let mut hasher = Blake2s256::new();
     hasher.update(b"A");
-    let res = hasher.finalize();
+    let res_a = hasher.finalize();
 
-    let arr1 : [u8; 4] = res[0..4].try_into().unwrap();
-    let arr2 : [u8; 4] = res[4..8].try_into().unwrap();
-    let arr3 : [u8; 4] = res[8..12].try_into().unwrap();
-    let arr4 : [u8; 4] = res[12..16].try_into().unwrap();
-    let arr5 : [u8; 4] = res[16..20].try_into().unwrap();
-    let arr6 : [u8; 4] = res[20..24].try_into().unwrap();
-    let arr7 : [u8; 4] = res[24..28].try_into().unwrap();
-    let arr8 : [u8; 4] = res[28..32].try_into().unwrap();
+    let arr1 : [u8; 4] = res_a[0..4].try_into().unwrap();
+    let arr2 : [u8; 4] = res_a[4..8].try_into().unwrap();
+    let arr3 : [u8; 4] = res_a[8..12].try_into().unwrap();
+    let arr4 : [u8; 4] = res_a[12..16].try_into().unwrap();
+    let arr5 : [u8; 4] = res_a[16..20].try_into().unwrap();
+    let arr6 : [u8; 4] = res_a[20..24].try_into().unwrap();
+    let arr7 : [u8; 4] = res_a[24..28].try_into().unwrap();
+    let arr8 : [u8; 4] = res_a[28..32].try_into().unwrap();
 
-    let res : [u32; 8] = [
+    let res_a : [u32; 8] = [
         u32::from_be_bytes(arr1),
         u32::from_be_bytes(arr2),
         u32::from_be_bytes(arr3), 
@@ -33,14 +33,15 @@ pub fn main() { // program without loops
         u32::from_be_bytes(arr8),
     ];
 
-    env::commit(&res);
+    env::commit(&res_a);
 
-    // let mut hasher = Blake2s256::new();
-    // hasher.update(b"B");
-    // let _res = hasher.finalize();
+    let mut hasher = Blake2s256::new();
+    hasher.update(b"B");
+    let res_b = hasher.finalize();
+    env::commit(&res_b);
 
-    // let mut hasher = Blake2s256::new();
-    // hasher.update(b"C");
-    // let _res = hasher.finalize();
-
+    let mut hasher = Blake2s256::new();
+    hasher.update(b"C");
+    let res_c = hasher.finalize();
+    env::commit(&res_c);
 }
--- cairo_programs/blake2s/blake2s.cairo
+++ cairo_programs/blake2s/blake2s.cairo
@@ -6,16 +6,34 @@ from starkware.cairo.common.cairo_builtins import BitwiseBuiltin
 
 func main{range_check_ptr, bitwise_ptr: BitwiseBuiltin*}() {
     alloc_locals;
-    let inputs: felt* = alloc();
 
-    assert inputs[0] = 'A';
+    let inputs_a: felt* = alloc();
+    assert inputs_a[0] = 'A';
 
-    let (local blake2s_ptr_start) = alloc();
-    let blake2s_ptr = blake2s_ptr_start;
-    let (output) = blake2s{range_check_ptr=range_check_ptr, blake2s_ptr=blake2s_ptr}(inputs, 1);
+    let (local blake2s_ptr_start_a) = alloc();
+    let blake2s_ptr_a = blake2s_ptr_start_a;
+    let (output) = blake2s{range_check_ptr=range_check_ptr, blake2s_ptr=blake2s_ptr_a}(inputs_a, 1);
     assert output.low = 283464815493723968210698558054580609432;
     assert output.high = 32682819570734175424590289175922030236;
 
-    finalize_blake2s(blake2s_ptr_start, blake2s_ptr);
+    finalize_blake2s(blake2s_ptr_start_a, blake2s_ptr_a);
+
+    let inputs_b: felt* = alloc();
+    assert inputs_b[0] = 'B';
+
+    let (local blake2s_ptr_start_b) = alloc();
+    let blake2s_ptr_b = blake2s_ptr_start_b;
+    let (output) = blake2s{range_check_ptr=range_check_ptr, blake2s_ptr=blake2s_ptr_b}(inputs_b, 1);
+
+    finalize_blake2s(blake2s_ptr_start_b, blake2s_ptr_b);
+
+    let inputs_c: felt* = alloc();
+    assert inputs_c[0] = 'C';
+
+    let (local blake2s_ptr_start_c) = alloc();
+    let blake2s_ptr_c = blake2s_ptr_start_c;
+    let (output) = blake2s{range_check_ptr=range_check_ptr, blake2s_ptr=blake2s_ptr_c}(inputs_c, 1);
+
+    finalize_blake2s(blake2s_ptr_start_c, blake2s_ptr_c);
     return ();
 }
